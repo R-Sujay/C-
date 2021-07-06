5af40318c200ea920433df61f345f029e0191fee
Rajesh G <info.rajesh@gmail.com>
2021-07-06 13:24:48 +0530

    slight editing

--- C++ Projects/number requester.cpp
+++ C++ Projects/number requester.cpp
@@ -6,7 +6,7 @@ int main()
 {
   int number = 0;
   int request;
-  cout << "Number Requester!";
+  cout << "Number Requester!\n";
   cout << "Enter Your Number: ";
   cin >> request;
   while (number < request) {
