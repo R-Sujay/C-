00d00b61930a2e1390fd7ca988cda6383308f8b7
Rajesh G <info.rajesh@gmail.com>
2021-07-06 16:56:15 +0530

    made character repeatition

--- C Projects/REPEAT STRING.c
+++ C Projects/REPEAT STRING.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
     char name[30];
     printf("Enter name:" );
     scanf("%s",name);
     printf("%s",name);
+    printf("\n\n\nPress any key to close this");
+    getch();
     return 0;
 }
---
+++ C++ Projects/Character repeater.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <conio.h>
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+string repeat(string s, int n)
+{
+	string s1 = s;
+
+	for (int i=1; i<n;i++)
+		s += s1; 
+
+	return s;
+}
+int main()
+{
+	string s;
+	int n;
+    cout << "Enter Your word(Enter One Word): ";
+    cin >> s;
+    cout << "Enter Number of Repeatation: ";
+    cin >> n;
+
+	cout << repeat(s,  n) << endl;;
+            cout << "\n\n\nPress any key to close";
+    getch();
+	return 0;
+}
