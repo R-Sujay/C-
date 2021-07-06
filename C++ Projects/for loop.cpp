36ae074f15303b43e14aef344771fbf7997a78cf
Rajesh G <info.rajesh@gmail.com>
2021-07-06 11:40:55 +0530

    test

---
+++ C++ Projects/for loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+  for (int i = 0; i < 5; i++) {
+    cout << i << "\n";
+  }
+          cout << "\n\n\nPress any key to close";
+    getch();
+  return 0;
+}
