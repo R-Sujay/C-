76529dbb5c8a3013e3586f1813943acf8162955f
R Sujay <sujay.rajesh08@gmail.com>
2021-07-12 11:40:29 +0530

    test

--- C++ Projects/fee Enquiry.cpp
+++ C++ Projects/fee Enquiry.cpp
@@ -137,7 +137,7 @@ int main()
         break;
     }
         cout << "\n\nThanks for making your payment!";
-        cout << "\n\n\nPress any key to close";
+        cout << "\n\nPress any key to close";
     getch();
     return 0;
 }
