d22c769cf619d3a117d96fdbd78a0967c96a111c
R Sujay <sujay.rajesh08@gmail.com>
2021-07-19 11:32:47 +0530

    test

--- C++/20 no. do whie loop.cpp
+++ C++/20 no. do whie loop.cpp
@@ -3,19 +3,22 @@ using namespace std;
 
 int main()
 {
-	int number;
-	// cout << "\nEnter Maximum limit Value to print Even Numbers =  ";
-	// cin >> number;
-	// cout << "\nList of Even Numbers from 1 to " << number << " are\n"; 
-	cout << "\nList of Even Numbers from 1 to 20" << " are\n"; 
-	// for(int i = 1; i <= number; i++)
-	for(int i = 1; i <= 20; i++)
-  	{
-  		if ( i % 2 == 0 )
-  		{
-  			cout << i <<" ";
-		}	
-  	}
-		
+    // int number;
+    // cout << "\nEnter Maximum limit Value to print Even Numbers =  ";
+    // cin >> number;
+while(1){
+
+do{ 
+
+for(int Even=0;Even<=20;Even+=2)
+// for(int Even=0;Even<=number;Even+=2)
+
+cout << Even<< endl;
+
+}while(0);
+
+break;
+
+}
  	return 0;
 }
